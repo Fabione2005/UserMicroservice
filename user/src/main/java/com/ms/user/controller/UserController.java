2c70e98f1705deff920de472c0b600f6fddb3f0a
Fabione <fabione2005@gmail.com>
2022-06-10 00:45:52 -0400

    cambiando url de endpoints

--- user/src/main/java/com/ms/user/controller/UserController.java
+++ user/src/main/java/com/ms/user/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.user.model.bean.User;
@@ -20,33 +21,34 @@ import com.ms.user.model.generic.UserToken;
 import com.ms.user.service.UserService;
 
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
 	@Autowired
 	UserService service;
 	
 	//Para este el body del request solo necesita email y password
-	@GetMapping(value = "/userAuth",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/auth",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserToken> login(@RequestBody UserToken user) {
 		return service.loginAuth(user);
 	}
 
-	@GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<User>> retrieveUsers() {
 		return service.retriveUsers();
 	}
 
-	@PostMapping(value = "/users/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> addUser(@RequestBody User User) {
 		return service.addUser(User);
 	}
 	
-	@PutMapping(value = "/users/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<BaseResult> updateUser(@RequestBody User user) {
 		return service.updateUser(user);
 	}
 	
-	@DeleteMapping(value = "/users/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<BaseResult> deleteUser(@PathVariable UUID id) {
 		return service.deleteUser(id);
 	}
